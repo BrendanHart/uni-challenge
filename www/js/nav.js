093600f585a5d3a92b651ac186d4a59860bd87f9
Brendan Hart <bxh538@student.bham.ac.uk>
2016-10-20 21:28:11 +0100

    Nav drawer working

---
+++ www/js/nav.js
@@ -0,0 +1,15 @@
+var nav = {
+    initialize: function() {
+        alert("test"); 
+    },
+
+    hide: function() {
+        alert("tett");
+    },
+
+    open: function() {
+        alert("tety");
+    }
+}
+
+nav.initialize();
